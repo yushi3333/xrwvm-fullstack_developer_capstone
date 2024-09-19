892aa17bbb71d882eb0202fa7747d88e8f48dffe
yushi3333 <a1125189291@gmail.com>
2024-09-19 13:03:20 -0400

    update

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,12 +11,40 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
+def get_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key,value in kwargs.items():
+            params = parms + key +"=" +value + "&"
+    request_url = backend_url + endpoints + "?" + params
+    print("GET from {}".format(request_url))
+    try:
+        response = requests.get(request_url)
+        return reponse.json()
+    except:
+        print("network exception occurred")
 # Add code for get requests to back end
 
 # def analyze_review_sentiments(text):
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
 # Add code for posting review
